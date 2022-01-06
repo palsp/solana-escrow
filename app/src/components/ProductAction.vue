194f82cef84839f6639f38c5c392e188797e9e5a
Supasin Liulak <palsp2406@gmail.com>
2022-01-06 12:13:10 +0700

    re: product's action area

---
+++ app/src/components/ProductAction.vue
@@ -0,0 +1,49 @@
+<template v-else>
+  <section v-if="showBuy" class="order-section">
+    <base-button @click="$emit('order')">BUY</base-button>
+  </section>
+  <section class="update-shipment-section" v-if="showUpdateShipment">
+    <div class="form-group">
+      <label for="trackingID">TRACKING ID : </label>
+      <input
+        type="text"
+        placeholder="tracking id......"
+        :value="trackingId"
+        @input="$emit('tracking-change', $event.target.value)"
+      />
+    </div>
+    <base-button @click="$emit('update-tracking')">UPDATE TRACKING</base-button>
+  </section>
+  <section v-if="showWithdraw" class="withdraw-section">
+    <base-button @click="$emit('withdraw')">Withdraw Fund</base-button>
+  </section>
+</template>
+
+<script>
+import { toRefs, context } from "vue";
+export default {
+  props: ["showBuy", "showUpdateShipment", "showWithdraw", "trackingId"],
+  emit: ["order", "update-tracking", "withdraw", "tracking-change"],
+  setup(props, context) {
+    return {
+      ...toRefs(props),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.update-shipment-section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-group {
+  margin: 2rem 0;
+}
+
+.form-group input {
+  padding: 0.5rem;
+}
+</style>
---
+++ app/src/components/ui/BaseButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 1rem 2rem;
+  border: none;
+  border-radius: 0.5rem;
+}
+
+.btn:hover {
+  background-color: #f391e3;
+  cursor: pointer;
+}
+</style>
